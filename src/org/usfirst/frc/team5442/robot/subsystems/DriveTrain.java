9d41fba4755a58d471948d65758405cea4eef55b
MJMauschbaugh <ilovencaa12@gmail.com>
2017-01-19 19:10:03 -0600

    4 Motor practice
    Made the code ready for stronghold bot

--- src/org/usfirst/frc/team5442/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5442/robot/subsystems/DriveTrain.java
@@ -9,23 +9,23 @@ public class DriveTrain extends Subsystem {
 	
 	private static Spark lSparkcontroller1 = RobotMap.dTSparkController1;
 	private static Spark lSparkController2 = RobotMap.dTSparkController2;
-	private static Spark lSparkController3 = RobotMap.dTSparkController3;
+
 	
 	private static Spark rSparkcontroller4 = RobotMap.dTSparkController4;
 	private static Spark rSparkController5 = RobotMap.dTSparkController5;
-	private static Spark rSparkController6 = RobotMap.dTSparkController6;
+
 
 	
 	public static void LeftSide(double speed){
 		lSparkcontroller1.set(speed);
 		lSparkController2.set(speed);
-		lSparkController3.set(speed);
+
 	}
 	
 	public static void RightSide(double speed){
 		rSparkcontroller4.set(speed);
 		rSparkController5.set(speed);
-		rSparkController6.set(speed);
+
 	}
 	@Override
 	protected void initDefaultCommand() {
