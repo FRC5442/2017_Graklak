111f723617244e763d7ec0026616e2ef92b267c3
SJBealmer <SJBealmer@wacohi.net>
2017-03-14 18:54:03 -0500

    Sorry this one's for real

--- src/org/usfirst/frc/team5442/robot/autoCommands/Blue_Boiler_Auto.java
+++ src/org/usfirst/frc/team5442/robot/autoCommands/Blue_Boiler_Auto.java
@@ -12,7 +12,7 @@ public class Blue_Boiler_Auto extends CommandGroup{
 	
 	public Blue_Boiler_Auto(){
 		addSequential(new DriveStraightCmd(9, -.5, 2));
-		addSequential(new TurnToAngleCmd(.25, -45));
+		addSequential(new TurnToAngleCmd(.3, 45));
 		addSequential(new Timer_Command(.5));
 		addSequential(new Drive_Command(13, .65, .65));
 		addSequential(new ShootAuto(10));
--- src/org/usfirst/frc/team5442/robot/autoCommands/Red_Boiler_Auto.java
+++ src/org/usfirst/frc/team5442/robot/autoCommands/Red_Boiler_Auto.java
@@ -12,7 +12,7 @@ public class Red_Boiler_Auto extends CommandGroup{
 
 	public Red_Boiler_Auto(){
 		addSequential(new DriveStraightCmd(9, -.5, 2));
-		addSequential(new TurnToAngleCmd(.25, -45));
+		addSequential(new TurnToAngleCmd(.3, -45));
 		addSequential(new Timer_Command(.5));
 		addSequential(new Drive_Command(13, .65, .65));
 		addSequential(new ShootAuto(10));
