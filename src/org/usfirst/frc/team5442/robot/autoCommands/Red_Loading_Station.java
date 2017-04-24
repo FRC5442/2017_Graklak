8ccd8728304ea18677d190aa37d38d116493db1a
MJMauschbaugh <ilovencaa12@gmail.com>
2017-04-24 15:27:43 -0500

    Added Run Down Field to Autos
    Preliminary

--- src/org/usfirst/frc/team5442/robot/autoCommands/Red_Loading_Station.java
+++ src/org/usfirst/frc/team5442/robot/autoCommands/Red_Loading_Station.java
@@ -16,5 +16,9 @@ public class Red_Loading_Station extends CommandGroup{
 		addSequential(new DriveStraightCmd(3.5, -.275, -.4, -1));
 		addSequential(new Gear_Arms_Out(1));
 		addSequential(new DriveStraightCmd(5, .4, .4, 1));
+		//new stuff
+		addParallel(new TurnToAngleCmd(.425, 51));
+		addSequential(new Timer_Command(2));
+		addSequential(new DriveStraightCmd(10, -4, 2, -1));
 	}
 }
