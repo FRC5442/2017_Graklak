17035e513d4b9e039ea55844cb587c820c228251
SJBealmer <SJBealmer@wacohi.net>
2017-04-26 22:54:49 -0500

    Testing
    I'm Calling it Good

---
+++ src/org/usfirst/frc/team5442/robot/subsystems/JoystickScaler.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5442.robot.subsystems;
+
+import org.usfirst.frc.team5442.robot.OI;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class JoystickScaler extends Subsystem {
+	
+	public static double Scale(int axisNumber){
+		double value = OI.xboxController1.getRawAxis(axisNumber);
+		if (value >= .8){
+			value = 1;
+		}
+		return value;
+	}
+	
+	protected void initDefaultCommand() {
+	}
+}
